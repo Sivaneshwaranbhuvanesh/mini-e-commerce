33a6b9a35036eaf4eded7be88511b0a14e8ce198
sivanesh59200322-hash <sivanesh59200322@gmail.com>
2026-04-07 15:58:10 +0530

    fix apache public folder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +18,17 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    
+public function boot(): void
+{
+    if (app()->environment('production')) {
+        try {
+            Artisan::call('migrate', [
+                '--force' => true,
+            ]);
+        } catch (\Exception $e) {
+            // prevent crash if migration fails
+        }
     }
+}
 }
